308999a96b668f1b80f1b4195b91d00df35d94f5
Denis Kerner <dtricks@live.de>
2021-07-16 14:36:03 +0200

    fix path stuff

--- devsetup.sh
+++ devsetup.sh
@@ -1,4 +1,6 @@
-export PATH="/home/dk/dev/openwrt/staging_dir/toolchain-*/bin):$PATH"
-	export STAGING_DIR="/home/dk/dev/openwrt/staging_dir"
+OPWD="${PWD}"
+cd /home/dk/dev/openwrt
+export STAGING_DIR="$PWD/staging_dir"
+export PATH="$PWD/$(echo staging_dir/toolchain-*/bin):$PATH"
 CC=mips-openwrt-linux-gcc
-CC=mips-openwrt-linux-gcc
+cd ${OPWD}
